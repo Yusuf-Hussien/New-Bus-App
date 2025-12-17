b9230bacc32f087526cdc8c98e4df89104450fbc
Ahmed <ai4456213@gmail.com>
2025-12-17 12:52:05 +0200

    merge: merged conflicts in front and back

--- Backend/NewBusProject/NewBusBLL/Exceptions/NotFoundException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NewBusBLL.Exceptions
-{
-    public class NotFoundException: Exception
-    {
-        public NotFoundException(string message) : base(message)
-        {
-        }
-    }
-}
